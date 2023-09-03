2b44e8c8736c056adbaca5332963af7d48f65c59
Infinity <gabriele.scorpaniti@outlook.com>
2023-09-03 19:59:06 +0200

    Updated Migration JS

--- migrations/1_full-deploy-goerli.js
+++ migrations/1_full-deploy-goerli.js
@@ -12,7 +12,7 @@ module.exports = async function(deployer) {
   catch(error)
   {
     console.error("Console Error: " + error)
-    var contract = await deployProxy(platform_contract, ["ipfs://bafkreihdmavgdtchrabhqifb6sm6rgqp4di4xd2y6gs73ukmmmy4zis6iq","0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"], "Test Contract", { deployer });
+    var contract = await deployProxy(platform_contract, ["ipfs://bafkreihdmavgdtchrabhqifb6sm6rgqp4di4xd2y6gs73ukmmmy4zis6iq",["0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"], "Test Contract"], { deployer });
   }
   
 };
