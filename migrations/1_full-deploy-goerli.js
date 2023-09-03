781c9d77c8a4e5564e97ba0562805c762ef943df
Infinity <gabriele.scorpaniti@outlook.com>
2023-09-03 19:48:33 +0200

    Fixed JS File

--- migrations/1_full-deploy-goerli.js
+++ migrations/1_full-deploy-goerli.js
@@ -12,7 +12,7 @@ module.exports = async function(deployer) {
   catch(error)
   {
     console.error("Console Error: " + error)
-    var contract = await deployProxy(platform_contract, ["","0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"], { deployer });
+    var contract = await deployProxy(platform_contract, ["","0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"], "", { deployer });
   }
   
 };
