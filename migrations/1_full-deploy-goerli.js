a2c5dd0de4367434bb77b23b14e2100090eea86c
Gabriele Scorpaniti <gabriele.scorpaniti@outlook.com>
2023-09-04 15:04:31 +0200

    Updated Pipeline and migration script

--- migrations/1_full-deploy-goerli.js
+++ migrations/1_full-deploy-goerli.js
@@ -4,6 +4,10 @@ var platform_contract = artifacts.require("standardNFT_DB");
 
 module.exports = async function(deployer) {
 
+  let contract_uri = "ipfs://bafkreihdmavgdtchrabhqifb6sm6rgqp4di4xd2y6gs73ukmmmy4zis6iq";
+  let contract_admins =  ["0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"];
+  let contract_name = "Test Contract";
+
   try
   {
     var contract = await platform_contract.deployed();
@@ -12,7 +16,7 @@ module.exports = async function(deployer) {
   catch(error)
   {
     console.error("Console Error: " + error)
-    var contract = await deployProxy(platform_contract, ["ipfs://bafkreihdmavgdtchrabhqifb6sm6rgqp4di4xd2y6gs73ukmmmy4zis6iq",["0xB312Dcf3Bd0BFEDf9c932C0f35fa1B3c3859e4a0"], "Test Contract"], { deployer });
+    var contract = await deployProxy(platform_contract, [contract_uri,contract_admins, contract_name], { deployer });
   }
   
 };
